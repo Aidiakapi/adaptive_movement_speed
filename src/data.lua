8819e2d847192cb201d15dc0cc58b538f67b3a4c
Aidiakapi <aidiakapi@gmail.com>
2024-10-22 10:34:01 +0200

    Update to Factorio 2.0

--- src/data.lua
+++ src/data.lua
@@ -10,29 +10,7 @@ data:extend({
         action = 'lua',
         toggleable = true,
         associated_control_input = 'adaptive-movement-speed-toggle-input',
-        icon =
-        {
-            filename = '__adaptive_movement_speed__/graphics/shortcut-bar.png',
-            priority = 'extra-high-no-scale',
-            size = 32,
-            scale = 1,
-            flags = {'icon'}
-        },
-        icon =
-        {
-            filename = '__adaptive_movement_speed__/graphics/shortcut-bar.png',
-            priority = 'extra-high-no-scale',
-            size = 32,
-            scale = 1,
-            flags = {'icon'}
-        },
-        disabled_icon =
-        {
-            filename = '__adaptive_movement_speed__/graphics/shortcut-bar-disabled.png',
-            priority = 'extra-high-no-scale',
-            size = 32,
-            scale = 1,
-            flags = {'icon'}
-        },
+        icon = '__adaptive_movement_speed__/graphics/shortcut-bar.png',
+        small_icon = '__adaptive_movement_speed__/graphics/shortcut-bar.png',
     }
 })
